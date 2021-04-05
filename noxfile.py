885537fd8e5cf282509b2cd2be4adf47153e0212
Dmitrii Rumyancev <rychanya@gmail.com>
2021-04-05 09:55:27 +0300

    nox

--- noxfile.py
+++ noxfile.py
@@ -2,5 +2,13 @@ import nox
 
 
 @nox.session(python=False)
-def black(session):
+def prety(session: nox.Session):
     session.run("black", ".")
+    session.run("isort", "--recursive", ".")
+
+
+@nox.session(python=False)
+def run_tests(session: nox.Session):
+    session.run("poetry", "shell")
+    session.run("poetry", "install")
+    session.run("pytest", "-v")
