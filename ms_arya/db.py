3e0366677d5a531d782b6848c428563b8843befa
Dmitrii Rumyancev <rychanya@gmail.com>
2021-04-11 12:09:01 +0300

    add add correct and incorrect

--- ms_arya/db.py
+++ ms_arya/db.py
@@ -1,25 +1,51 @@
-from os import getenv
 from typing import Optional
 
-from dotenv import load_dotenv
 from pymongo import MongoClient
-from pymongo.collection import Collection
-from pymongo.errors import ConnectionFailure
+from pymongo.collection import Collection, ReturnDocument
 
-load_dotenv()
+from ms_arya import config
+from ms_arya.models import QA
 
-URI = getenv("ARYA_URI", None)
-DB_NAME = getenv("ARYA_DB_NAME", "TEST_DB")
-COLLECTION_NAME = getenv("ARYA_COLLECTION_NAME", "TEST_COLLECTION")
+client = MongoClient(config.DB_URI)
 
 
 def get_collection() -> Optional[Collection]:
-    try:
-        db = client.get_database(DB_NAME)
-        collection = db.get_collection(COLLECTION_NAME)
-        return collection
-    except ConnectionFailure:
-        return None
+    db = client.get_database(config.DB_NAME)
+    collection = db.get_collection(config.DB_COLLECTION_NAME)
+    return collection
 
 
-client = MongoClient(URI)
+def find_or_create(qa: QA) -> QA:
+    filter = {
+        "question": qa.question,
+        "type": qa.type,
+        "answers": {"$all": [{"$elemMatch": {"$eq": answer}} for answer in qa.answers]},
+    }
+    update = {"$setOnInsert": {"answers": qa.answers}}
+    if qa.type == QA.type_enum.join:
+        filter.update(
+            {
+                "extra_answers": {
+                    "$all": [
+                        {"$elemMatch": {"$eq": answer}} for answer in qa.extra_answers
+                    ]
+                }
+            }
+        )
+        update["$setOnInsert"].update({"extra_answers": qa.extra_answers})
+    data = get_collection().find_one_and_update(
+        filter=filter, update=update, upsert=True, return_document=ReturnDocument.AFTER
+    )
+    return QA.parse_obj(data)
+
+
+def add_correct_answer(qa: QA, answer):
+    filter = qa.dict(include={"id"}, by_alias=True)
+    update = {"$set": {"correct": answer}}
+    get_collection().find_one_and_update(filter=filter, update=update)
+
+
+def add_incorrect_answer(qa: QA, answer):
+    filter = qa.dict(include={"id"}, by_alias=True)
+    update = {"$addToSet": {"incorrect": answer}}
+    get_collection().find_one_and_update(filter=filter, update=update)
