a0fcc05a045745c37ba6ee6b6bed8ded5613a7a4
Dmitrii Rumyancev <rychanya@gmail.com>
2021-04-13 17:59:55 +0300

    add docs session

--- noxfile.py
+++ noxfile.py
@@ -1,15 +1,28 @@
+import shutil
+
 import nox
+
+nox.options.sessions = ["prety", "tests"]
 
 
 @nox.session(python=False)
 def prety(session: nox.Session):
+    session.run("poetry", "shell")
     session.run("black", ".")
     session.run("isort", "--recursive", ".")
     session.run("flake8", ".")
 
 
 @nox.session(python=False)
-def run_tests(session: nox.Session):
+def tests(session: nox.Session):
     session.run("poetry", "shell")
     session.run("poetry", "install")
     session.run("pytest", "-v")
+
+
+@nox.session(python=False)
+def docs(session: nox.Session):
+    session.run("poetry", "shell")
+    session.chdir("docs")
+    shutil.rmtree("_build", ignore_errors=True)
+    session.run("sphinx-build", "-b", "html", "-W", ".", "_build/html")
