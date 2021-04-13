ca8c6c03b8becb43769ae14b4b151ea88d26cec9
Dmitrii Rumyancev <rychanya@gmail.com>
2021-04-13 17:15:33 +0300

    add save_answer instead property is_correct

--- ms_arya/machine.py
+++ ms_arya/machine.py
@@ -9,7 +9,6 @@ class QAMachine:
         self.qa: QA = find_or_create(qa)
         assert self.qa is not None
         self._answer = None
-        self._is_correct = None
 
     @property
     def answer(self):
@@ -17,14 +16,8 @@ class QAMachine:
             self._answer = get_answer(self.qa)
         return self._answer
 
-    @property
-    def is_correct(self):
-        return self._is_correct
-
-    @is_correct.setter
-    def is_correct(self, value):
-        if value:
+    def save_answer(self, is_correct: bool):
+        if is_correct:
             add_correct_answer(self.qa, self.answer)
         else:
             add_incorrect_answer(self.qa, self.answer)
-        self._is_correct = bool(value)
